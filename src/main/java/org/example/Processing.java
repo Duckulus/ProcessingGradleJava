895e33808731c3624f61f9690885eb571987dab4
Amin <amin.haddou@frg.wwschool.de>
2022-05-21 18:43:00 +0200

    add windows, macOS and linux native files to support all renderers/graphics modes

--- src/main/java/org/example/Processing.java
+++ src/main/java/org/example/Processing.java
@@ -1,12 +1,12 @@
 package org.example;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
 
 public class Processing extends PApplet {
-
     @Override
     public void settings() {
-        size(640, 360);
+        size(640, 360, PConstants.JAVA2D);
     }
 
     @Override
